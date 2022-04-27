acf54bc025e8b4587ab955ca8be605ae8152c972
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2022-04-27 21:01:07 +0100

    Updated Swagger doc

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/rest/TemplateRenditionResource.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/rest/TemplateRenditionResource.java
@@ -39,15 +39,7 @@ public class TemplateRenditionResource {
 
     @Operation(
         summary = "Renders a templates using provided values and uploads it to Document Store."
-            + " secureDocStoreEnabled attribute is disabled by default.",
-            parameters = {
-                    @Parameter(in = ParameterIn.HEADER, name = "authorization",
-                            description = "Authorization (Idam Bearer token)", required = true,
-                            schema = @Schema(type = "string")),
-                    @Parameter(in = ParameterIn.HEADER, name = "serviceauthorization",
-                            description = "Service Authorization (S2S Bearer token)", required = true,
-                            schema = @Schema(type = "string"))}
-    )
+            + " secureDocStoreEnabled attribute is disabled by default.")
     @PostMapping("/template-renditions")
     public ResponseEntity<CreateTemplateRenditionDto> createTemplateRendition(
             @RequestBody @Valid CreateTemplateRenditionDto createTemplateRenditionDto,
