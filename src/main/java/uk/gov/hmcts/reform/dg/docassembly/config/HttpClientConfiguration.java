948e9d4a4b4af5f4fe6ed93755783139cffe1185
MUSTAFA <mdayican@gmail.com>
2022-10-11 10:40:37 +0100

    EM-4648-setup max connection

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/config/HttpClientConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/config/HttpClientConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.dg.docassembly.config;
 
+import okhttp3.Dispatcher;
 import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +14,22 @@ public class HttpClientConfiguration {
     @Value("${httpclient.call-timeout}")
     private int timeout;
 
+    @Value("${httpclient.connection-timeout}")
+    private int connectionTimeout;
+
     @Bean
     public OkHttpClient okHttpClient() {
-        return new OkHttpClient.Builder()
-            .connectTimeout(timeout, TimeUnit.SECONDS)
-            .readTimeout(timeout, TimeUnit.SECONDS)
-            .writeTimeout(timeout, TimeUnit.SECONDS)
-            .build();
+
+        Dispatcher dispatcher = new Dispatcher();
+        dispatcher.setMaxRequests(100);
+        dispatcher.setMaxRequestsPerHost(100);
+        return new OkHttpClient
+                .Builder()
+                .connectTimeout(connectionTimeout, TimeUnit.SECONDS)
+                .readTimeout(timeout, TimeUnit.SECONDS)
+                .writeTimeout(timeout, TimeUnit.SECONDS)
+                .dispatcher(dispatcher)
+                .build();
     }
 
 }
