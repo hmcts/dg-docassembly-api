b198c1c9ac13bbbd82b4736f895a206ddd554d38
mdayican <mustafa.dayican@hmcts.net>
2022-10-15 09:29:27 +0100

    EM-4666 checkstyle

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/config/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/config/security/SecurityConfiguration.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.dg.docassembly.config.security;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
