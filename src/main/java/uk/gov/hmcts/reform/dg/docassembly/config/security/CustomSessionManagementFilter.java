c2783275175d2e86f40ec3ea12b37a830e7a9a60
mdayican <mdayican@gmail.com>
2024-04-18 16:09:31 +0100

    use NullSecurityContextRepository

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/config/security/CustomSessionManagementFilter.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/config/security/CustomSessionManagementFilter.java
@@ -4,7 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.NullSecurityContextRepository;
 import org.springframework.security.web.session.SessionManagementFilter;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +13,8 @@ import java.io.IOException;
 @Component
 public class CustomSessionManagementFilter extends SessionManagementFilter {
 
-    public CustomSessionManagementFilter(HttpSessionSecurityContextRepository securityContextRepository) {
-        super(securityContextRepository);
+    public CustomSessionManagementFilter() {
+        super(new NullSecurityContextRepository());
     }
 
     @Override
