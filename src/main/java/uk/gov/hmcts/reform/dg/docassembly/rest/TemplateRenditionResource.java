cd3ef7d2e7c04e4756e04fa37505b586c773f89f
lars lunde <lars.lunde@hmcts.net>
2024-10-14 14:50:59 +0100

    removed unused import

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/rest/TemplateRenditionResource.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/rest/TemplateRenditionResource.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
