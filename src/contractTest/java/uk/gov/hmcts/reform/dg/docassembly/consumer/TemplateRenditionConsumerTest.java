795c998445cc085ae77a13ca89e25c9ee78753ac
michaeljacob <Michael.Jacob@HMCTS.NET>
2025-07-14 15:21:31 +0100

    add pact test

---
+++ src/contractTest/java/uk/gov/hmcts/reform/dg/docassembly/consumer/TemplateRenditionConsumerTest.java
@@ -0,0 +1,85 @@
+package uk.gov.hmcts.reform.dg.docassembly.consumer;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.consumer.dsl.LambdaDslObject;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.V4Pact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import io.restassured.http.ContentType;
+import lombok.extern.slf4j.Slf4j;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+
+import static au.com.dius.pact.consumer.dsl.LambdaDsl.newJsonBody;
+
+@Slf4j
+@PactTestFor(providerName = "doc_assembly_template_rendition_provider")
+class TemplateRenditionConsumerTest extends BaseConsumerTest {
+
+    private static final String TEMPLATE_RENDITION_PROVIDER_NAME = "doc_assembly_template_rendition_provider";
+    private static final String TEMPLATE_RENDITION_API_PATH = "/api/template-renditions";
+
+    @Pact(provider = TEMPLATE_RENDITION_PROVIDER_NAME, consumer = DOC_ASSEMBLY_CONSUMER)
+    public V4Pact createTemplateRendition200(PactDslWithProvider builder) {
+        return builder
+            .given("a template can be rendered successfully")
+            .uponReceiving("A request to create a template rendition")
+            .path(TEMPLATE_RENDITION_API_PATH)
+            .method(HttpMethod.POST.toString())
+            .headers(getHeaders())
+            .body(createRequestDsl())
+            .willRespondWith()
+            .status(HttpStatus.OK.value())
+            .body(createResponseDsl())
+            .toPact(V4Pact.class);
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "createTemplateRendition200")
+    void testCreateTemplateRendition200(MockServer mockServer) {
+        SerenityRest
+            .given()
+            .headers(getHeaders())
+            .contentType(ContentType.JSON)
+            .body(createRequestDsl().getBody().toString())
+            .post(mockServer.getUrl() + TEMPLATE_RENDITION_API_PATH)
+            .then()
+            .statusCode(HttpStatus.OK.value());
+    }
+
+    private DslPart createRequestDsl() {
+        return newJsonBody(this::buildRequestBody).build();
+    }
+
+    private void buildRequestBody(LambdaDslObject body) {
+        body
+            .stringType("templateId", "FL-FRM-GOR-ENG-12345")
+            .booleanType("secureDocStoreEnabled", true)
+            .stringType("caseTypeId", "FinancialRemedyContested")
+            .stringType("jurisdictionId", "DIVORCE")
+            .stringType("hashToken", "Abcde12345")
+            .stringMatcher("outputType", "(PDF|DOCX)", "PDF")
+            .object("formPayload", p -> p
+                .stringType("caseReference", "1234-5678-9012-3456")
+                .stringType("applicantFirstName", "John")
+                .stringType("applicantLastName", "Smith")
+            )
+            .array("errors");
+    }
+
+    private DslPart createResponseDsl() {
+        return newJsonBody(this::buildResponseBody).build();
+    }
+
+    private void buildResponseBody(LambdaDslObject body) {
+        buildRequestBody(body);
+        body.stringType(
+            "renditionOutputLocation",
+            "http://dm-store:8080/documents/a1b2c3d4-e5f6-7890-g1h2-i3j4k5l6m7n8"
+        );
+    }
+}
---
+++ src/contractTest/java/uk/gov/hmcts/reform/dg/docassembly/provider/TemplateRenditionProviderTest.java
@@ -0,0 +1,77 @@
+package uk.gov.hmcts.reform.dg.docassembly.provider;
+
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import uk.gov.hmcts.reform.dg.docassembly.dto.CreateTemplateRenditionDto;
+import uk.gov.hmcts.reform.dg.docassembly.dto.RenditionOutputType;
+import uk.gov.hmcts.reform.dg.docassembly.rest.TemplateRenditionResource;
+import uk.gov.hmcts.reform.dg.docassembly.service.TemplateRenditionService;
+import uk.gov.hmcts.reform.dg.docassembly.service.exception.DocumentTaskProcessingException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@Provider("doc_assembly_template_rendition_provider")
+@WebMvcTest(value = TemplateRenditionResource.class, excludeAutoConfiguration = {
+    SecurityAutoConfiguration.class,
+    OAuth2ClientAutoConfiguration.class
+})
+@TestPropertySource(properties = "endpoint-toggles.enable-secure-document-templ-rend-endpoint=true")
+public class TemplateRenditionProviderTest extends BaseProviderTest {
+
+    @Autowired
+    private TemplateRenditionResource templateRenditionResource;
+
+    @MockitoBean
+    private TemplateRenditionService templateRenditionService;
+
+    @Override
+    protected Object[] getControllersUnderTest() {
+        return new Object[]{templateRenditionResource};
+    }
+
+    @State({"a template can be rendered successfully"})
+    public void createTemplateRendition() throws DocumentTaskProcessingException, IOException {
+        CreateTemplateRenditionDto mockResponseDto = createMockResponseDto();
+
+        when(templateRenditionService.renderTemplate(any(CreateTemplateRenditionDto.class)))
+            .thenReturn(mockResponseDto);
+    }
+
+    private CreateTemplateRenditionDto createMockResponseDto() {
+        CreateTemplateRenditionDto dto = new CreateTemplateRenditionDto();
+        dto.setTemplateId("FL-FRM-GOR-ENG-12345");
+        dto.setSecureDocStoreEnabled(true);
+        dto.setCaseTypeId("FinancialRemedyContested");
+        dto.setJurisdictionId("DIVORCE");
+        dto.setHashToken("Abcde12345");
+        dto.setOutputType(RenditionOutputType.PDF);
+        dto.setErrors(new ArrayList<>());
+        dto.setRenditionOutputLocation("http://dm-store:8080/documents/a1b2c3d4-e5f6-7890-g1h2-i3j4k5l6m7n8");
+
+        try {
+            ObjectMapper localObjectMapper = new ObjectMapper();
+            JsonNode formPayload = localObjectMapper.readTree(
+                "{\"caseReference\":\"1234-5678-9012-3456\","
+                + "\"applicantFirstName\":\"John\","
+                + "\"applicantLastName\":\"Smith\"}"
+            );
+            dto.setFormPayload(formPayload);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create mock form payload", e);
+        }
+
+        return dto;
+    }
+}
