bbd22f4f09962d62e3ceb72187118796925528c2
mdayican <mdayican@gmail.com>
2026-01-13 09:14:46 +0000

    fix Pr comment

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/Application.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/Application.java
@@ -2,8 +2,6 @@ package uk.gov.hmcts.reform.dg.docassembly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 @SpringBootApplication(
     scanBasePackages = {
@@ -11,7 +9,6 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
         "uk.gov.hmcts.reform.dg.docassembly",
         "uk.gov.hmcts.reform.auth"}
 )
-@EnableFeignClients(basePackageClasses = {IdamApi.class})
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
     public static void main(String[] args) {
