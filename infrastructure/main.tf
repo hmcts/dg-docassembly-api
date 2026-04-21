237c1eeaa1b27f4b171706e1ce2354d2de798357
manohar-hmcts <127882722+manohar-hmcts@users.noreply.github.com>
2026-04-21 11:18:08 +0100

    DTSPO-30212: add Jenkins managed identity keyvault access for dg-docassembly-api (#2427)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -21,12 +21,18 @@ resource "azurerm_resource_group" "rg" {
   tags     = local.tags
 }
 
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 module "local_key_vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   product                 = local.app_full_name
   env                     = var.env
   tenant_id               = var.tenant_id
   object_id               = var.jenkins_AAD_objectId
+  jenkins_object_id       = data.azurerm_user_assigned_identity.jenkins.principal_id
   resource_group_name     = azurerm_resource_group.rg.name
   product_group_object_id = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
   common_tags             = var.common_tags
