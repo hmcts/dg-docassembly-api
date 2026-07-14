6e6193ac0b9b5995c72cb921591cc93a0f3d33ef
mdayican <mustafa.dayican1@hmcts.net>
2026-07-14 14:53:46 +0100

    EM-7234 use new jenkins version (#2554)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -27,16 +27,17 @@ data "azurerm_user_assigned_identity" "jenkins" {
 }
 
 module "local_key_vault" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  product                 = local.app_full_name
-  env                     = var.env
-  tenant_id               = var.tenant_id
-  object_id               = var.jenkins_AAD_objectId
-  jenkins_object_id       = data.azurerm_user_assigned_identity.jenkins.principal_id
-  resource_group_name     = azurerm_resource_group.rg.name
-  product_group_object_id = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
-  common_tags             = var.common_tags
-  create_managed_identity = true
+  source                       = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
+  product                      = local.app_full_name
+  env                          = var.env
+  tenant_id                    = var.tenant_id
+  object_id                    = var.jenkins_AAD_objectId
+  jenkins_object_id            = data.azurerm_user_assigned_identity.jenkins.principal_id
+  resource_group_name          = azurerm_resource_group.rg.name
+  product_group_object_id      = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
+  common_tags                  = var.common_tags
+  create_managed_identity      = true
+  grant_preview_jenkins_access = var.env == "aat"
 }
 
 provider "vault" {
