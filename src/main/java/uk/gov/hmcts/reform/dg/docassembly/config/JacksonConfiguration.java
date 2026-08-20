776d66da9000ebb295e6feab16c4e86ea91c66e0
mdayican <mdayican@gmail.com>
2026-08-20 16:19:15 +0100

    remove BlackbirdModule

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/config/JacksonConfiguration.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.dg.docassembly.config;
 
-import com.fasterxml.jackson.module.blackbird.BlackbirdModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.problem.violations.ConstraintViolationProblemModule;
@@ -8,13 +7,6 @@ import org.zalando.problem.violations.ConstraintViolationProblemModule;
 @Configuration
 public class JacksonConfiguration {
 
-    /*
-     * Jackson Afterburner module to speed up serialization/deserialization.
-     */
-    @Bean
-    public BlackbirdModule blackbirdModule() {
-        return new BlackbirdModule();
-    }
 
     /*
      * Module for serialization/deserialization of ConstraintViolationProblem.
